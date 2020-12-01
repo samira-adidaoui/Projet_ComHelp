0ad1a4c0e3b8f3b0d182ea57d8cb3c401a5aea79
Arby Brice <brice.arby91@gmail.com>
2020-12-02 00:20:28 +0100

    routes+other

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   belongs_to :city
+       has_many :created_posts, class_name: 'Post'
        has_many :participations
+			 has_many :answered_posts, through: :participations, source: :post
        validates :first_name, presence: true
        validates :last_name, presence: true
          
