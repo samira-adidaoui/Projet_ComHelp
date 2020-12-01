7fce2af6b97dfcc6c684e877859b44aa5b2e3f38
Arby Brice <brice.arby91@gmail.com>
2020-12-01 11:38:19 +0100

    Last change to team/index.html.erb + routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
+  
+  get 'orders/index'
+  root 'products#index'
+  
   devise_for :users
-	root 'posts#index'
-  resources :posts
- 	resources :team, only: [:index] 
+	resources :contact, only: [:index]
+  resources :products
+	resources :team, only: [:index]
+
+  resources :users, only: [:show] do
+    resources :carts, only: [:show] do
+      resources :charges, only: [:new, :create]
+    end
+  end
+
+ 
+  
+
+  resources :product_carts, only: [:new, :create, :destroy]
 end
