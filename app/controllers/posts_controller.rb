efa9565a0a3e1412036a4b2a228c65fa81a86313
todplaz <balocm@yahoo.fr>
2020-12-02 23:37:31 +0100

    edit delete first commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,52 @@ class PostsController < ApplicationController
     @participant = Participation.where(:post_id => @post.id)  
 
   end
+  
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to_posts_path notice: "Post envoyé avec succès!"
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if  @post.update_attributes(post_params)
+      redirect_to posts_path, notice => "Votre post a été mis a jour"
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, :notice => "Votre post a été supprimé"
+  end
+
+  def owned_post  
+    unless current_user == @post.user
+      flash[:alert] = "Vous n'etes pas propriétaire de ce post!"
+      redirect_to root_path
+      
+    end  
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
+
 end
