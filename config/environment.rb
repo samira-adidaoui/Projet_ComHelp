4114a5a1ea08e268b94e686ef0bacdb71c1ceea8
todplaz <balocm@yahoo.fr>
2020-12-06 21:56:02 +0100

    test mailer-doing

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,10 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_LOGIN'],
-  :password => ENV['SENDGRID_PWD'],
+  :user_name => 'apikey',
+  :password => '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',
   :domain => 'monsite.fr',
   :address => 'smtp.sendgrid.net',
   :port => 587,
