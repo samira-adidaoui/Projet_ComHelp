b1f0cc4ebdd8ad7b7d75aa29756513235ba00a3c
Arby Brice <brice.arby91@gmail.com>
2020-12-07 22:23:49 +0100

    Automatic tweet of new ads OK

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
+require 'twitter'
+
 class Post < ApplicationRecord
-    after_create :notif_new
+    after_create :email_to_admin, :automatic_tweet
 
     belongs_to :user
     belongs_to :category
@@ -11,9 +13,19 @@ class Post < ApplicationRecord
     validates :title, presence: true, length: { in: 5..140 }
     validates :description, presence: true, length: { in: 20..1000}
 
-    def notif_new
-	AdminMailer.new_ad_notification(self).deliver_now
+    def email_to_admin
+			AdminMailer.new_ad_notification(self).deliver_now
     end
+
+	def automatic_tweet
+    client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+    config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+    config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+  end
+    client.update("Annonce postée à #{self.city.city_name} : #{self.description}")
+	end
     
 =begin
     include AlgoliaSearch
