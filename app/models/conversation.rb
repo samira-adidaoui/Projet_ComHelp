a353480dc1e75785aa17824fd6420c1fcacb4617
marine <marinekouadio@gmail.com>
2020-12-07 13:24:52 +0100

    private message update

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -5,7 +5,16 @@ class Conversation < ApplicationRecord
   
     validates_uniqueness_of :sender_id, scope: :receiver_id
   
-    scope :between, -> (sender_id,receiver_id) do
+    scope :between, -> (sender_id, receiver_id) do
       where("(conversations.sender_id = ? AND conversations.receiver_id = ?) OR (conversations.receiver_id = ? AND conversations.sender_id = ?)", sender_id, receiver_id, sender_id, receiver_id)
     end
+  
+    def recipient(current_user)
+      self.sender_id == current_user.id ? self.receiver : self.sender
+    end
+  
+    def unread_message_count(current_user)
+      self.messages.where("user_id != ? AND read = ?", current_user.id, false).count
+    end
+  
   end
