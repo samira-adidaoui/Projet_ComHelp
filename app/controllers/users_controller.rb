543c37dcbbf056cf6233f4c7e3d9dcfabec16bc5
todplaz <balocm@yahoo.fr>
2020-12-07 20:57:45 +0100

    rating commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,17 @@
 class UsersController < ApplicationController
+  before_action :only : [:upvote, :downvote] 
   def show
     @user = User.find(params[:id])
+  end
+
+  def upvote
+    @user.upvote_from current_user
+    redirect_to posts_path
+  end
+
+  def downvote
+    @user.downvote_from current_user
+    redirect_to posts_path
   end
 
 end
