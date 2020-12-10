8618acc4b5963c290d065543e9b39a494fb2f3a0
marine <marinekouadio@gmail.com>
2020-12-10 14:44:24 +0100

    add message button in user page

--- db/schema.rb
+++ db/schema.rb
@@ -81,11 +81,9 @@ ActiveRecord::Schema.define(version: 2020_12_07_113020) do
     t.string "first_name"
     t.string "last_name"
     t.integer "notation"
-    t.bigint "city_id"
     t.boolean "is_admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["city_id"], name: "index_users_on_city_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
