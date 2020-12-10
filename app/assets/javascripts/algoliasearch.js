0118deb7eaab1f8aa56a700aae3105c683f8ffd2
Samira Adidaoui <samira.adidaoui@gmail.com>
2020-12-11 00:00:21 +0100

    algolia frontend searching

--- app/assets/javascripts/algoliasearch.js
+++ app/assets/javascripts/algoliasearch.js
@@ -1,7 +1,8 @@
-const searchClient = algoliasearch('XRDT1NI3ZK','fbf1725475ec1845c71f24b6ee7fdf89');
+
+const searchClient =algoliasearch('XRDT1NI3ZK','fbf1725475ec1845c71f24b6ee7fdf89');
 
 const search = instantsearch({
-  indexName: 'post_development',
+  indexName: 'Post_development',
   searchClient,
 });
 
@@ -20,16 +21,7 @@ search.addWidgets([
     container: '#hits',
     templates: {
       item: `
-        <div>
-          <img src="{{image}}" align="left" alt="{{name}}" />
-          <div class="hit-name">
-            {{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}
-          </div>
-          <div class="hit-description">
-            {{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}
-          </div>
-          <div class="hit-price">\${{price}}</div>
-        </div>
+        
       `,
     },
   }),
