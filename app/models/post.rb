7b406d21cdb49d0564829d4f662ee1c9480c4c16
marine <marinekouadio@gmail.com>
2020-12-11 18:03:49 +0100

    update

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 require 'twitter'
 
 class Post < ApplicationRecord
-   after_create :email_to_admin
-		after_commit :automatic_tweet
+  #  after_create :email_to_admin
+	# 	after_commit :automatic_tweet
 
     belongs_to :user
     belongs_to :category
@@ -14,22 +14,26 @@ class Post < ApplicationRecord
     validates :title, presence: true, length: { in: 5..140 }
     validates :description, presence: true, length: { in: 20..1000}
 
-    def email_to_admin
-        AdminMailer.new_ad_notification(self).deliver_now
-     end
+    # def notif_new
+	  # AdminMailer.new_ad_notification(self).deliver_now
+    # end
+
+    # def email_to_admin
+    #     AdminMailer.new_ad_notification(self).deliver_now
+    #  end
     
-    def automatic_tweet
-        if self.is_validated == true
+    # def automatic_tweet
+    #     if self.is_validated == true
     
-        client = Twitter::REST::Client.new do |config|
-        config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-        config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-        config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-        config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
-      end
-        client.update("Annonce postée à #{self.city.city_name} : #{self.description}")
-      end
-    end
+    #     client = Twitter::REST::Client.new do |config|
+    #     config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+    #     config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+    #     config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+    #     config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+    #   end
+    #     client.update("Annonce postée à #{self.city.city_name} : #{self.description}")
+    #   end
+    # end
 end
 
 
