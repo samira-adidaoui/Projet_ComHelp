62474c979806201492e80a63af6c6df859cd34b7
Samira Adidaoui <samira.adidaoui@gmail.com>
2020-12-11 16:09:52 +0100

    algolia updated

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,16 +1,10 @@
 class AdminMailer < ApplicationMailer
-<<<<<<< HEAD
-=begin
 	  default from: 'alexsteph22@gmail.com'
-=======
-	  default from: 'alexsteph22@gmail.com
-	  '
->>>>>>> develop
 
 	def new_ad_notification(post)
 
 		@post=post
 		mail(to: 'admin_comhelp@yopmail.com', subject: 'Une nouvelle annonce a été postée')
 	end
-=end
+
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-=begin
   default from: 'from@example.com'
   layout 'mailer'
-=end
+
 end
